7eb39128a58e5ca08c7356921097e2b9732eb56f
taarushv <taarushv15@gmail.com>
2016-05-30 16:26:49 +0530

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from flask import Flask , render_template , request , redirect
 import sqlite3
-import random
+
+
+basedigits='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+BASE=len(basedigits)
+'''BASE 62 ENNCODIG FOR NUMBERS TO STRINGS'''
+
+def decode(s):
+    ret,mult = 0,1
+    for c in reversed(s):
+        ret += mult*basedigits.index(c)
+        mult *= BASE
+    print(ret)
+
+def encode(num):
+    if num <0: raise Exception("positive number "+num)
+    if num ==0: return '0'
+    ret=''
+    while num != 0:
+        ret = (basedigits[num%BASE])+ret
+        num = int(num/BASE)
+    print(ret)
 
 app = Flask(__name__)
 long_url = 0
@@ -11,55 +31,58 @@ def index():
 
 @app.route('/longurl', methods=['GET','POST'])
 def username():
+
     global long_url
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
     long_url = request.form['longurl']
+    c.execute("INSERT INTO stuff (longurls) values (?)", (long_url,))
+    conn.commit()
+    c.close()
+    conn.close()
     return render_template("test.html", long_url= long_url)
 
-set1 = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','$']
-set2 = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','$']
-conn = sqlite3.connect('databas101.db')
-c = conn.cursor()
-small_url_edge = str(random.choice(set1)) + str(random.choice(set2))
-final_short_url = 'http://127.0.0.1:5000/' + small_url_edge
-for x in range(10):
-    c.execute("SELECT longurl FROM stuff WHERE shorturl=?", (final_short_url,))
-    existing_data = c.fetchall()
-    if existing_data == []:
-        pass
-    else:
-        small_url_edge = str(random.choice(set1)) + str(random.choice(set2))
-        final_short_url = 'http://127.0.0.1:5000/' + small_url_edge
+
 
 
 @app.route('/smallurl',methods=['GET','POST'])
 
 def shortessturl():
-    conn = sqlite3.connect('databas101.db')
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    c.execute("INSERT INTO stuff (longurl, shorturl) values (?, ?)",(long_url,final_short_url))
-    conn.commit()
-    c.close()
-    conn.close()
+    lurl = str(long_url)
+
+    c.execute("SELECT id FROM stuff WHERE longurls=?",(lurl,))
+    k = c.fetchone()
+    for shit in k:
+        m = shit
+    if m <0: raise Exception("positive number "+m)
+    if m ==0: return '0'
+    ret=''
+    while m != 0:
+        ret = (basedigits[m%BASE])+ret
+        m = int(m/BASE)
+    shortcode = ret
+    final_short_url = 'http://127.0.0.1:5000/'+ str(shortcode)
+
+    return render_template("shorturl.html", final_short_url=final_short_url)
 
 
-    return render_template("shorturl.html", final_short_url= final_short_url)
 
-
-
-@app.route('/<shorts>')
-def finalstep(shorts):
-
-
-    conn = sqlite3.connect('databas101.db')
+@app.route('/<shortcode>')
+def finalstep(shortcode):
+    ret,mult = 0,1
+    for c in reversed(shortcode):
+        ret += mult*basedigits.index(c)
+        mult *= BASE
+    assign = ret
+    conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    final_short_url = str("http://127.0.0.1:5000/")+ str(shorts)
-    c.execute("SELECT * FROM stuff WHERE shorturl=?", (final_short_url,))
+    c.execute("SELECT * FROM stuff WHERE id=?", (assign,))
     for row in c.fetchall():
-        longesturl = row[0]
+        longesturl = row[1]
         return redirect(longesturl)
-@app.route('/hello')
-def shit():
-    return "hfgiuahfvp uis"
+
 if __name__ == "__main__":
     app.run(debug=True)
 
@@ -68,7 +91,7 @@ if __name__ == "__main__":
 
 
 
-
+encode (5)
 
 
 
